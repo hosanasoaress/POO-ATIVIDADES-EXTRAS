23ed9eee488dbacd120a661584e660bac5f8322e
hosanasoaress <hosana.clara@aluno.ifce.edu.br>
2024-10-29 13:41:52 -0300

    Add files via upload

---
+++ ValoresVariaveis/src/br/edu/principal/Principal.java
@@ -0,0 +1,15 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int value = 10;
+		char x;
+		x = 'A';
+		System.out.println(value);
+		System.out.println("The value of x = " + x);
+
+		
+	}
+
+}
