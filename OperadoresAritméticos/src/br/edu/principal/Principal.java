ebe05442ea52302c3a034809903c6d2dc4fd20a0
hosanasoaress <hosana.clara@aluno.ifce.edu.br>
2024-10-29 13:43:04 -0300

    Add files via upload

---
+++ OperadoresAritméticos/src/br/edu/principal/Principal.java
@@ -0,0 +1,34 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int i = 37;
+		int j = 42;
+		double x = 27.475;
+		double y = 7.22;
+		System.out.println("Valor das variáveis...");
+		System.out.println("i = " + i);
+		System.out.println("j = " + j);
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+		
+		System.out.println("Adição...");
+		System.out.println("i + j =" + (i + j));
+		System.out.println("x + y =" + (x + y));
+		
+		System.out.println("Subtração...");
+		System.out.println("i - j =" + (i - j));
+		System.out.println("x - y =" + (x - y));
+		
+		System.out.println("Multiplicação...");
+		System.out.println("i * j =" + (i * j));
+		System.out.println("x * y =" + (x * y));
+		
+		System.out.println("Divisão...");
+		System.out.println("i / j = " + (i / j));
+		System.out.println("x / y =" + (x / y));
+
+	}
+
+}
